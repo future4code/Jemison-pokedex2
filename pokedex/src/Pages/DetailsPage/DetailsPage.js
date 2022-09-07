2c8f346e24d5e2df77755aad3a9757b82bfcc50d
RenatoAlexandrini <re.alexandrini@gmail.com>
2022-09-06 21:57:42 -0300

    homePage

--- pokedex/src/Pages/DetailsPage/DetailsPage.js
+++ pokedex/src/Pages/DetailsPage/DetailsPage.js
@@ -1,12 +1,17 @@
 import React from "react";
-import AppBarDetailsPage from "../AppBar/AppBarDetailsPage";
+import { useNavigate } from "react-router-dom";
+import { goToHome } from "../../Routes/coordinator";
+import { goToPokedex } from "../../Routes/coordinator";
+import * as Stl from './DetailsPageStl.js';
 
 function DetailsPage() {
-    return(
-        <div>
-            <AppBarDetailsPage/>
-            DetailsPage
-        </div>
+
+    const navigate = useNavigate();
+    return (
+        <Stl.Main>
+            <button onClick={() => goToHome(navigate)}>HomePage</button>
+            <button onClick={() => goToPokedex(navigate)}>Pokedex</button>
+        </Stl.Main>
     )
 }
 
